072ee68402fc78249bf86e6f58cd092f9359074e
Tukhtasinov-Saydullo <tukhtasinov.saydullo@yahoo.com>
2023-06-01 19:56:17 +0500

    UPD! Added New Models and Serializers + Views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from .models import CustomUserModel
+from .serializers import UserRegisterSerializer, UserLoginToken
+
 
 # Create your views here.
+
+class UserRegisterView(generics.CreateAPIView):
+    queryset = CustomUserModel
+    serializer_class = UserRegisterSerializer
+
+
+class UserLoginView(TokenObtainPairView):
+    serializer_class = UserLoginToken
